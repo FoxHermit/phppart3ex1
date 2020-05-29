ea46896792276995ac0e28688e5dd2cef41c09ca
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-05-29 18:46:33 +0200

    corrigé

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$months = array();
+
+$months[0] = 'Janvier';
+$months[1] = 'Février';
+$months[2] = 'Mars';
+$months[3] = 'Avril';
+$months[4] = 'Mai';
+$months[5] = 'Juin';
+$months[6] = 'Juillet';
+$months[7] = 'Aout';
+$months[8] = 'Septembre';
+$months[9] = 'Octobre';
+$months[10] = 'Novembre';
+$months[11] = 'Décembre';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 1</title>
+  </head>
+  <body>
+  </body>
+</html>
